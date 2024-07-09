b33efb24abb7445ecee3ed030a8dd3ce0a90cc62
Paolo Sacconier <paolo@sacconier.net>
2024-07-10 00:24:48 +0200

    feat: schemathesis API tests (#7)
    * feat: schemathesis tests added

--- finn_codesubmit/router.py
+++ finn_codesubmit/router.py
@@ -1,5 +1,7 @@
 import os
+from fastapi import HTTPException
 from openapi_to_fastapi.routes import SpecRouter
+from pydantic import BaseModel
 
 from finn_codesubmit.datastore import InMemoryDataStore
 from finn_codesubmit.url_shortener import UrlShortener
@@ -14,16 +16,34 @@ spec_router = SpecRouter(api_spec_path)
 url_shortener = UrlShortener(url_prefix=url_prefix, datastore=InMemoryDataStore())
 
 
-@spec_router.post("/encode")
+class ErrorResponseModel(BaseModel):
+    detail: str
+
+
+def name_factory(path: str, **kwargs):
+    return path.replace("/", " ").strip()
+
+
+@spec_router.post(
+    "/encode", name_factory=name_factory, responses={400: {"model": ErrorResponseModel}}
+)
 async def encode(url: str):
-    encodedUrl = await url_shortener.shorten_url(url)
-    return {"shortUrl": encodedUrl}
+    try:
+        encodedUrl = await url_shortener.shorten_url(url)
+        return {"shortUrl": encodedUrl}
+    except KeyError:
+        raise HTTPException(detail=f"Bad URL: {url}", status_code=400)
 
 
-@spec_router.post("/decode")
+@spec_router.post(
+    "/decode", name_factory=name_factory, responses={400: {"model": ErrorResponseModel}}
+)
 async def decode(url: str):
-    originalUrl = await url_shortener.get_long_url(url)
-    return {"originalUrl": originalUrl}
+    try:
+        originalUrl = await url_shortener.get_long_url(url)
+        return {"originalUrl": originalUrl}
+    except KeyError:
+        raise HTTPException(detail=f"Bad short URL: {url}", status_code=400)
 
 
 router = spec_router.to_fastapi_router()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import schemathesis
 from fastapi.testclient import TestClient
 
 from finn_codesubmit.app import app
@@ -22,3 +23,27 @@ def test_decode_endpoint():
     assert "originalUrl" in response.json()
     assert isinstance(response.json()["originalUrl"], str)
     assert response.json()["originalUrl"] == "https://www.google.com"
+
+
+def test_invalid_encode_long_url():
+    response = client.post("/encode/", params={"url": "this is not an url"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Bad URL: this is not an url"}
+
+
+def test_invalid_decode_short_url():
+    response = client.post(
+        "/decode/", params={"url": "https://wrongservice.dev/49CcFSu5tcoFwK"}
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "Bad short URL: https://wrongservice.dev/49CcFSu5tcoFwK"
+    }
+
+
+schema = schemathesis.from_asgi("/openapi.json", app)
+
+
+@schema.parametrize()
+def test_api(case):
+    case.call_and_validate()
